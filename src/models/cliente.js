12b330cd2dfbe0d05b6e5ee856ad2419991d4fb7
juliaAraujodev <63320916+szya5@users.noreply.github.com>
2023-02-15 08:32:45 -0300

    Add files via upload

---
+++ src/models/cliente.js
@@ -0,0 +1,9 @@
+class Cliente {
+    constructor(id_cliente, nome, cpf, email, senha){
+        this.id_cliente = id_cliente
+        this.nome = nome
+        this.email = email
+        this.senha = senha
+    }
+}
+module.exports = Cliente
