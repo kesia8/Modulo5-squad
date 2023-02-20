4ea4d2c36e7d7af477ece18e9fe7f621b4abff42
juliaAraujodev <63320916+szya5@users.noreply.github.com>
2023-02-20 14:29:26 -0300

    Add files via upload

--- src/infra/bd.js
+++ src/infra/bd.js
@@ -7,6 +7,8 @@ const bdCliente =[]
 const bdFornecedor =[]
 const bdFuncionarios =[]
 
+//cliente//
+
 const cliente = new Cliente(1, 'Eugenio oliveira', 'eugenio.oliveira@bol.com.br', 'eugeninhooliveira')
 bdCliente.push(cliente)
 
@@ -16,14 +18,19 @@ bdCliente.push(cliente2)
 const cliente3 = new Cliente(3, 'Mirtes Faria Lima', 'mirtes_fl@yahoo.com', 'aboboradedoce')
 bdCliente.push(cliente3)
 
+//funcionarios//
+
 const funcionarios = new Funcionarios (1, 'Eugenio oliveira', '15416512326', 'eugenio.oliveira@bol.com.br', 'eugeninhooliveira', 'Gerente Administrativo', new Date())
-bdFuncionarios.push(Funcionarios)
+bdFuncionarios.push(funcionarios)
 
 const funcionarios2 = new Funcionarios(2, 'Oliveira Ribeiro', '23457895412', 'oliveira.ribeiro@gmail.com', 'ribeirinho', 'Diretora', new Date())
 bdFuncionarios.push(funcionarios2)
 
 const funcionarios3 = new Funcionarios(3, 'Mirtes Faria Lima', '75964812543', 'mirtes_fl@yahoo.com', 'aboboradedoce', 'Gerente Financeiro', new Date())
 bdFuncionarios.push(funcionarios3)
+
+
+//fornecedores//
 
 const fornecedores = new Fornecedores(222055446881, 'Alan Ferreira', 'Belo Horizonte', 'Minas Gerais', new Date())
 bdFornecedor.push(fornecedores)
