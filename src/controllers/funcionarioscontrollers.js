634a4bdb52f1821833899dd78a5ce94a61aee68d
juliaAraujodev <63320916+szya5@users.noreply.github.com>
2023-02-23 17:06:22 -0300

    Update funcionarioscontrollers.js

--- src/controllers/funcionarioscontrollers.js
+++ src/controllers/funcionarioscontrollers.js
@@ -1,15 +1,9 @@
 import { bdFuncionarios } from '../infra/bd.js'
-import Funcionarios from '../models/funcionarios.js'
 
 class funcionariosController {
 
     static rotas(app) {
         app.get('/funcionarios', funcionariosController.listar)
-        app.get('/funcionarios/id/:id', funcionariosController.buscarPorID)
-        app.post('/funcionarios', funcionariosController.inserir)
-        app.put('/funcionarios/id/:id', funcionariosController.atualizarfuncionarios)
-        app.delete('/funcionarios/id/:id', funcionariosController.deletar)
-
     }
 
     //ROTA GET
@@ -17,70 +11,6 @@ class funcionariosController {
 
         res.send(bdFuncionarios)
     }
-
-    //ROTA GET de BUSCAR
-    static buscarPorID(req, res) {
-        const funcionarios = bdFuncionarios.find(funcionarios => funcionarios.id === req.params.id)
-
-        //se for vazio(se não cliente)  
-        if (!funcionarios) {
-            res.send("Funcionarios não encontrado")
-            return
-        }
-
-        res.send(funcionarios)
-    }
-
-    //ROTA POST
-    static inserir(req, res) {
-        const funcionarios = new Funcionarios(req.body.id, req.body.nome, req.body.CPF, req.body.endereco )
-        bdFuncionarios.push(Funcionarios)
-        res.send(bdFuncionarios)
-        console.log(req.body)
-    }
-
-
-    //ROTA PUT
-
-    static atualizaFuncionario(req, res) {
-        const funcionarios = bdFuncionarios.find(funcionarios => funcionarios.id === req.params.id)
-
-        if (!funcionarios) {
-            res.send('funcionario não encontrado')
-            // res.status(404).send('Funcionario não encontrado')
-            return 
-        }
-
-        funcionarios.id = req.body.id
-        funcionarios.nome = req.body.nome
-        funcionarios.CPF = req.body.CPF
-        funcionarios.endereco = req.body.endereco
-        
-        
-        // res.status(200).send(bdFuncionarios)
-        res.send(bdFuncionarios)
-    }
-
-
-
-    // ROTA DELETE
-    static deletar(req, res) {
-        const funcionarios = bdFuncionarios.find(funcionarios => funcionarios.id === req.params.id)
-
-        if (!funcionarios) {
-            res.send("funcionario não encontrado")
-            return
-        }
-
-        const index = bdFuncionarios.indexOf(funcionarios);
-        bdFuncionarios.splice(index, 1);
-        res.send({
-            "Mensagem": `O funcionario do id ${funcionarios.id} foi deletado!`
-        });
-    }
-
-
 }
-
 
 export default funcionariosController
