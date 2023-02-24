1d25a60c55e555d8774dd12539bd824786a5645f
Silvia Corrêa <112557591+scorrea03@users.noreply.github.com>
2023-02-24 11:39:09 -0300

    Update fornecedorescontrollers.js

--- src/controllers/fornecedorescontrollers.js
+++ src/controllers/fornecedorescontrollers.js
@@ -11,6 +11,69 @@ class fornecedoresController {
 
         res.send(bdFornecedores)
     }
+//ROTA GET de BUSCAR
+    static buscarPorCod(req, res) {
+        const Fornecedor = bdFornecedor.find(Fornecedor => Fornecedor.cod === req.params.cod)
+
+        //se for vazio(se não cliente)  
+        if (!Fornecedor) {
+            res.send("Fornecedor não encontrado")
+            return
+        }
+
+        res.send(Fornecedor)
+    }
+
+    //ROTA POST
+    static inserir(req, res) {
+        const Fornecedor = new Fornecedor(req.body.cod, req.body.nome, req.body.cidade, req.body.estado )
+        bdFornecedor.push(Fornecedor)
+        res.send(bdFornecedor)
+        console.log(req.body)
+    }
+
+
+    //ROTA PUT
+
+    static atualizarFornecedor(req, res) {
+        const Fornecedor = bdFornecedor.find(Fornecedor => Fornecedor.cod === req.params.cod)
+
+        if (!Fornecedor) {
+            res.send('Fornecedor não encontrado')
+            // res.status(404).send('Fornecedor não encontrado')
+            return 
+        }
+
+        Fornecedor.cod = req.body.cod
+        Fornecedor.nome = req.body.nome
+        Fornecedor.cidade = req.body.cidade
+        Fornecedor.estado = req.body.estado
+        
+        
+        // res.status(200).send(bdFornecedor)
+        res.send(bdFornecedor)
+    }
+
+
+
+    // ROTA DELETE
+    static deletar(req, res) {
+        const cliente = bdFornecedor.find(Fornecedor => Fornecedor.cod === req.params.cod)
+
+        if (!Fornecedor) {
+            res.send("Fornecedor não encontrado")
+            return
+        }
+
+        const index = bdFornecedor.indexOf(Fornecedor);
+        bdFornecedor.splice(index, 1);
+        res.send({
+            "Mensagem": `O fornecedor do código ${Fornecedor.cod} foi deletado!`
+        });
+    }
+
+
 }
 
-export default fornecedoresController
+
+export default fornecedorController
